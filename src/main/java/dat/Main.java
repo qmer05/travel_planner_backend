7816d7a75b24906de131c45f6a3a101884944588
Ømer <cph-oo203@cphbusiness.dk>
2024-10-09 14:09:16 +0200

    added populate to main

--- src/main/java/dat/Main.java
+++ src/main/java/dat/Main.java
@@ -1,6 +1,7 @@
 package dat;
 
 import dat.config.ApplicationConfig;
+import dat.config.Populate;
 import dat.controllers.impl.ExceptionController;
 import dat.exceptions.ApiException;
 import dat.exceptions.Message;
@@ -14,5 +15,6 @@ public class Main {
 
     public static void main(String[] args) {
         ApplicationConfig.startServer(7070);
+        Populate.main(args);
     }
 }
