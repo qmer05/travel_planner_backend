7c3b91d1e35e88546be0c442ca565dce924a414b
Ømer <cph-oo203@cphbusiness.dk>
2024-10-23 12:15:58 +0200

    testing watchtower update

--- src/main/java/dat/Main.java
+++ src/main/java/dat/Main.java
@@ -2,14 +2,6 @@ package dat;
 
 import dat.config.ApplicationConfig;
 import dat.config.Populate;
-import dat.controllers.impl.ExceptionController;
-import dat.exceptions.ApiException;
-import dat.exceptions.Message;
-import dat.routes.Routes;
-import io.javalin.Javalin;
-import io.javalin.http.Context;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class Main {
 
