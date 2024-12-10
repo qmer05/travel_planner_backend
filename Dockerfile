705cc325c5f9ecb5a5c0ba7eb69d3b019b45e571
Ømer <cph-oo203@cphbusiness.dk>
2024-12-10 13:15:10 +0100

    changed port from 7010 to 7060

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ FROM amazoncorretto:17-alpine
 COPY target/app.jar /app.jar
 
 # Expose the port your app runs on
-EXPOSE 7010
+EXPOSE 7060
 
 # Command to run your app
 CMD ["java", "-jar", "/app.jar"]
--- src/main/java/dat/Main.java
+++ src/main/java/dat/Main.java
@@ -6,7 +6,7 @@ import dat.config.Populate;
 public class Main {
 
     public static void main(String[] args) {
-        ApplicationConfig.startServer(7010);
+        ApplicationConfig.startServer(7060);
         Populate.main(args);
     }
 }
