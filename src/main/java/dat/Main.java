aa290d2b70fafb1d140553ccaad99e1f7c75e1d0
Ømer <cph-oo203@cphbusiness.dk>
2024-12-11 09:44:15 +0100

    creating tables

--- src/main/java/dat/Main.java
+++ src/main/java/dat/Main.java
@@ -7,6 +7,6 @@ public class Main {
 
     public static void main(String[] args) {
         ApplicationConfig.startServer(7060);
-        // Populator.populate();
+        Populator.populate();
     }
 }
